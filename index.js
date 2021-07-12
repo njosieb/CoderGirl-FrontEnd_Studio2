3d607440aac9f91b797f0a01fa0c9e25d271c21a
Katie Blackstone <katierblackstone@gmail.com>
2021-07-12 11:40:29 -0500

    Rearranged index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function getData() {
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then(function(response) {
+      return response.json()
+    }) 
+    .then(function(data) {
+      renderBios(data);
+    })
+}
 
 //Function should return a component displaying an astronaut's bio
 function AstronautBios(astronaut) {
@@ -9,19 +18,15 @@ function AstronautBios(astronaut) {
   )
 }
 
-function getData() {
-  fetch("https://handlers.education.launchcode.org/static/astronauts.json")
-    .then(function(response) {
-      return response.json()
-    }) 
-    .then(function(data) {
-      //Render all astronaut bios. 
-      //The AstronautBios component should be repeated to display bios for all 
-      //astronaunts in the returned data object.
-      
-      //Then attach to the 'root' div!
-    })
+// Function should render all astronaut bios
+function renderBios(data) {
+  //The AstronautBios component should be repeated to display bios for all 
+  //astronaunts in the returned data object.
+  
+  //Then attach to the 'root' div!
 }
+
+
 
 getData();
 
