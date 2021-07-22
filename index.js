42044260799cc609d7cb42d220759c8b8e1c7860
njosieb <ninaharris121@gmail.com>
2021-07-21 21:42:20 -0500

    Completes Studio 2

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ function getData() {
     }) 
     .then(function(data) {
       renderBios(data);
+
     })
 }
 
 //Function should return a component displaying an astronaut's bio
 function AstronautBios(astronaut) {
-
+    
   return (
-    <div>
-
+    <div className="astronaut">
+      <div>
+        <h3>{astronaut.firstName} { astronaut.lastName }</h3>
+        <ul>
+          <li>{ astronaut.hoursInSpace }</li>
+          <li>{ astronaut.active.toString() }</li>
+          <li>{ astronaut.skills }</li>
+        </ul>
+      </div>
+      <img className="avatar" src={ astronaut.picture }/>
     </div>
   )
 }
@@ -22,8 +31,19 @@ function AstronautBios(astronaut) {
 function renderBios(data) {
   //The AstronautBios component should be repeated to display bios for all 
   //astronaunts in the returned data object.
+
   
   //Then attach to the 'root' div!
+  const root = document.getElementById('root');
+  const container = <div className='container'>
+    {AstronautBios(data[0])}
+    {AstronautBios(data[1])}
+    {AstronautBios(data[2])}
+    {AstronautBios(data[3])}
+    {AstronautBios(data[4])}
+                    </div>
+  ReactDOM.render( container, root );
+
 }
 
 
